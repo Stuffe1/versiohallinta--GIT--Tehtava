4abf3210f6386e5099cd1aa9bee34b8643fffb24
Stuffe <Juuuhooo10@gmail.com>
2025-12-03 12:32:11 +0200

    Luvut 1 ja 2 lisätty

--- Versiohallinta (GIT) -tehtävä/src/App.java
+++ Versiohallinta (GIT) -tehtävä/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("TESTI");
+
+        int luku1 = 5;
+        int luku2 = 10;
+
+        System.out.println(luku1 + luku2);
+    
     }
 }
