3381aa763c45a563e7208ff20609c1c7a01bd589
RobinPuntari <robin.puntari@gmail.com>
2025-12-04 16:31:46 +0200

    Update App.java

--- Versiohallinta (GIT) -tehtävä/src/App.java
+++ Versiohallinta (GIT) -tehtävä/src/App.java
@@ -2,9 +2,10 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         int luku1 = 5;
-        int luku2 = 10;
+        int luku2 = 5;
 
         System.out.println(luku1 + luku2);
+        
     
     }
 }
